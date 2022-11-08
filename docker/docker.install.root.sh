4c9f2e3df590971b1dcd741861c2cbd0171b1bf9
gnuhub <gnuhub@gmail.com>
2022-11-08 17:13:42 +0800

    auto commit

--- docker/docker.install.root.sh
+++ docker/docker.install.root.sh
@@ -26,6 +26,9 @@ curl https://raw.githubusercontent.com/arch4edu/mirrorlist/master/mirrorlist.arc
 sed -i 's/^#Server/Server/' /etc/pacman.d/arch4edu-mirrorlist
 cat /etc/pacman.d/arch4edu-mirrorlist
 
+curl https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist > /etc/pacman.d/endeavouros-mirrorlist
+
+
 pacman-key --init
 # chaotic
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
