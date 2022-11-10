557e31edf0599d625528c9bb31364ece3a266b86
gnuhub <gnuhub@gmail.com>
2022-11-10 11:03:04 +0800

    auto commit

--- 6.ci.sh
+++ 6.ci.sh
@@ -29,8 +29,8 @@ cat /home/runner/.ssh/known_hosts
 cd ~/
 git clone git@github.com:archlinux365/9318-archlinux-docker.git
 
-docker run ghcr.io/${GITHUB_REPOSITORY}/$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER pacman -Sl > packages.list.all.txt
-docker run ghcr.io/${GITHUB_REPOSITORY}/$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER pacman -Qe > packages.list.installed.txt
+# docker run ghcr.io/${GITHUB_REPOSITORY}/$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER pacman -Sl > packages.list.all.txt
+# docker run ghcr.io/${GITHUB_REPOSITORY}/$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER pacman -Qe > packages.list.installed.txt
 
 git add .
 git commit -a -m "$GITHUB_REF_NAME:$GITHUB_RUN_NUMBER"
