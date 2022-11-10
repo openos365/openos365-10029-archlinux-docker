a91357599f6b80c5ff93ce169c2e638caefb81c2
gnuhub <gnuhub@gmail.com>
2022-11-11 00:18:46 +0800

    auto commit

--- 5.update.packages.list.sh
+++ 5.update.packages.list.sh
@@ -8,6 +8,6 @@ export TERM=xterm-256color
 
 echo $PROJECT_NAME
 cd $CMD_PATH
-sudo docker pull ghcr.io/archlinux365/9318-archlinux-docker/root:latest
-sudo docker run ghcr.io/archlinux365/9318-archlinux-docker/root:latest pacman -Sl > packages.list.all.txt
-sudo docker run ghcr.io/archlinux365/9318-archlinux-docker/root:latest pacman -Qe > packages.list.installed.txt
+sudo docker pull ghcr.io/archlinux365/9318-archlinux-docker-root:latest
+sudo docker run ghcr.io/archlinux365/9318-archlinux-docker-root:latest pacman -Sl > packages.list.all.txt
+sudo docker run ghcr.io/archlinux365/9318-archlinux-docker-root:latest pacman -Qe > packages.list.installed.txt
